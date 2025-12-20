5e9b7f885326c0e8f160b92493de07cdd89f068e
bala-amypo <bala@amypo.in>
2025-12-20 16:16:14 +0000

    User commit

--- src/main/java/com/example/demo/repository/UsagePatternModelRepository.java
+++ src/main/java/com/example/demo/repository/UsagePatternModelRepository.java
@@ -1,12 +1,9 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.Bin;
 import com.example.demo.model.UsagePatternModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
+import java.util.List;
 
 public interface UsagePatternModelRepository extends JpaRepository<UsagePatternModel, Long> {
-
-    Optional<UsagePatternModel> findTop1ByBinOrderByLastUpdatedDesc(Bin bin);
+    List<UsagePatternModel> findByBinIdOrderByCreatedAtDesc(Long binId);
 }
