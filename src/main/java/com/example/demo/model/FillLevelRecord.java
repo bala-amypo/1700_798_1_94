3a3122018a3693e5e17191c2072a68de0c7999b6
bala-amypo <bala@amypo.in>
2025-12-20 15:27:12 +0000

    User commit

--- src/main/java/com/example/demo/model/FillLevelRecord.java
+++ src/main/java/com/example/demo/model/FillLevelRecord.java
@@ -1,54 +1,16 @@
 package com.example.demo.model;
 
-import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import java.sql.Timestamp;
 
-@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FillLevelRecord {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @ManyToOne
-    @JoinColumn(name = "bin_id", nullable = false)
     private Bin bin;
-
-    private Double fillPercentage;
-
-    private Timestamp recordedAt;
-
-    // Getter and setter for 'bin'
-    public Bin getBin() {
-        return bin;
-    }
-
-    public void setBin(Bin bin) {
-        this.bin = bin;
-    }
-
-    // Other getters and setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Double getFillPercentage() {
-        return fillPercentage;
-    }
-
-    public void setFillPercentage(Double fillPercentage) {
-        this.fillPercentage = fillPercentage;
-    }
-
-    public Timestamp getRecordedAt() {
-        return recordedAt;
-    }
-
-    public void setRecordedAt(Timestamp recordedAt) {
-        this.recordedAt = recordedAt;
-    }
+    private double fillLevelPercentage;
+    private Timestamp createdAt;
 }
