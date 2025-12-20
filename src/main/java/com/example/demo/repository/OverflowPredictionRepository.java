4ecb2c40a6f40e3392bbf0b079aa9edc92df1db5
bala-amypo <bala@amypo.in>
2025-12-20 15:43:21 +0000

    User commit

--- src/main/java/com/example/demo/repository/OverflowPredictionRepository.java
+++ src/main/java/com/example/demo/repository/OverflowPredictionRepository.java
@@ -1,3 +1,5 @@
+package com.example.demo.repository;
+
 import com.example.demo.model.OverflowPrediction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
