d901aff5c4fcc3985c13767cfafb00e6772ea81f
bala-amypo <bala@amypo.in>
2025-12-20 15:34:53 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/FillLevelRecordServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/FillLevelRecordServiceimpl.java
@@ -7,11 +7,11 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class FillLevelRecordServiceImpl implements FillLevelRecordService {
+public class FillLevelRecordServiceimpl implements FillLevelRecordService {
 
     private final FillLevelRecordRepository fillLevelRecordRepository;
 
-    public FillLevelRecordServiceImpl(FillLevelRecordRepository fillLevelRecordRepository) {
+    public FillLevelRecordServiceimpl(FillLevelRecordRepository fillLevelRecordRepository) {
         this.fillLevelRecordRepository = fillLevelRecordRepository;
     }
 
