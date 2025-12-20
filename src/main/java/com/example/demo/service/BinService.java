d108b46007a1ca9363f030960ae6df93c1753055
bala-amypo <bala@amypo.in>
2025-12-20 12:29:31 +0000

    User commit

--- src/main/java/com/example/demo/service/BinService.java
+++ src/main/java/com/example/demo/service/BinService.java
@@ -1,12 +1,12 @@
 package com.example.demo.service;
 
-import com.example.demo.model.Bin;
 import java.util.List;
+import com.example.demo.model.Bin;
 
 public interface BinService {
     Bin createBin(Bin bin);
-    Bin updateBin(Long id, Bin bin);
-    Bin getBinById(Long id);
     List<Bin> getAllBins();
+    Bin getBinById(Long id);
+    Bin updateBin(Long id, Bin bin);
     void deactivateBin(Long id);
 }
