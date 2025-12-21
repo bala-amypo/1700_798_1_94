d9168e12e7a2b512fb2464e3c83ed16f91760719
bala-amypo <bala@amypo.in>
2025-12-21 15:34:45 +0000

    User commit

--- src/main/java/com/example/demo/model/Bin.java
+++ src/main/java/com/example/demo/model/Bin.java
@@ -18,22 +18,50 @@ public class Bin {
     @ManyToOne
     private Zone zone;
 
-    // ===== Getters =====
-    public Long getId() { return id; }
-    public String getIdentifier() { return identifier; }
-    public String getLocationDescription() { return locationDescription; }
-    public Double getLatitude() { return latitude; }
-    public Double getLongitude() { return longitude; }
-    public Double getCapacityLiters() { return capacityLiters; }
-    public boolean isActive() { return active; }
-    public Zone getZone() { return zone; }
 
-    // ===== Setters =====
-    public void setIdentifier(String identifier) { this.identifier = identifier; }
-    public void setLocationDescription(String locationDescription) { this.locationDescription = locationDescription; }
-    public void setLatitude(Double latitude) { this.latitude = latitude; }
-    public void setLongitude(Double longitude) { this.longitude = longitude; }
-    public void setCapacityLiters(Double capacityLiters) { this.capacityLiters = capacityLiters; }
-    public void setActive(boolean active) { this.active = active; }
-    public void setZone(Zone zone) { this.zone = zone; }
+    public Long getId() {
+     return id; 
+     }
+    public String getIdentifier() { 
+        return identifier; 
+        }
+    public String getLocationDescription() {
+     return locationDescription; 
+     }
+    public Double getLatitude() {
+         return latitude; 
+         }
+    public Double getLongitude() {
+     return longitude; 
+     }
+    public Double getCapacityLiters() { 
+        return capacityLiters; 
+        }
+    public boolean isActive() {
+         return active; 
+         }
+    public Zone getZone() {
+         return zone;
+          }
+
+    public void setIdentifier(String identifier) {
+     this.identifier = identifier;
+      }
+    public void setLocationDescription(String locationDescription) { 
+        this.locationDescription = locationDescription; 
+        }
+    public void setLatitude(Double latitude) { 
+        this.latitude = latitude;
+         }
+    public void setLongitude(Double longitude) { 
+        this.longitude = longitude;
+         }
+    public void setCapacityLiters(Double capacityLiters) {
+         this.capacityLiters = capacityLiters;
+          }
+    public void setActive(boolean active) {
+         this.active = active; 
+         }
+    public void setZone(Zone zone) {
+         this.zone = zone; }
 }
