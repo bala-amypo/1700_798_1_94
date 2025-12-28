389b5fa29cfbf80ae8b040eaa671bb228da5e653
bala-amypo <bala@amypo.in>
2025-12-28 12:01:13 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -11,13 +11,21 @@ public class AuthResponse {
         this.role = role;
     }
     
-    // Getters and Setters
-    public String getToken() { return token; }
-    public void setToken(String token) { this.token = token; }
+    public String getToken() {
+     return token;
+      }
+    public void setToken(String token) { 
+    this.token = token; 
+    }
     
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getEmail() {
+     return email;
+      }
+    public void setEmail(String email) {
+     this.email = email;
+      }
     
-    public String getRole() { return role; }
+    public String getRole() {
+     return role; }
     public void setRole(String role) { this.role = role; }
 }
